0e25c0b180224fbe7b68fb7e953050ed6410c030
Daniele Castagna <daniele.castagna@outlook.com>
2023-11-20 09:47:39 +0200

    11.15-11.17 valmis?

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('10 tag action toimii? v0.1.0 -> v0.1.1') // change this string to ensure a new version deployed
+  res.send('11 tag action toimii?') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
